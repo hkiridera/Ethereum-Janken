5b28f5f9f0edab0827c7a0e4b4a0f6fc747d4aad
hkiridera <migratory39@gmail.com>
2017-09-02 12:47:32 +0900

    インデントをそろえる

--- Janken.sol
+++ Janken.sol
@@ -3,34 +3,28 @@ pragma solidity ^0.4.0;
 // bet is wei
 // if you send wei, you can get double with the probability of 1/3.
 // and, It triples with a probability of 1/7.
-
 contract Janken {
-    
     address owner;
     address contract_address;
     
     event Send(address from, address to, uint value);
-    
     function Janken() {
         owner = msg.sender;
         contract_address = address(this);
     }
-    
-    
     //Return balance of the contract 
-    function get_balance() constant returns (uint){
+    function getBalance() constant returns (uint) {
         return contract_address.balance;
     }
-    
     //Execute when remitting to the contract
-    function () payable{
+    function () payable {
         // check your bet
         uint bet = msg.value;
 
         // if you bet more than contract balance, finish this program.
-        if ( get_balance() /4 < bet){
-            msg.sender.send(bet);
-        }else{
+        if ( getBalance()/4 < bet) {
+            msg.sender.transfer(bet);
+        } else {
             //get chalenger address.
             address challenger = msg.sender;
         
@@ -39,23 +33,27 @@ contract Janken {
         
             //Jactpot
             //If it is divisible 7 times, it will be tripled
-            if(rnd % 7  == 0){
-                msg.sender.send(bet * 3);
+            if ( rnd % 7 == 0 ) {
+                msg.sender.transfer(bet*3);
             }
         
             //If it is divisible 3 times, it will be douobles
-            if(rnd % 3  == 0){
+            if ( rnd % 3 == 0 ) {
                 // send wei
-                if (challenger.send( bet * 2 )){
+                if ( challenger.send(bet*2)) {
                     //win event
                     Send(contract_address, challenger, bet * 2);
                 }
-            }else{
+            } else {
                 //lose event
                 Send(contract_address, challenger, 0);
             }
         }
     }
     // delete smartcontract
-    function kill() { if (msg.sender == owner) selfdestruct(owner); }
+    function kill() {
+        if (msg.sender == owner) {
+            selfdestruct(owner);
+        }
+     }
 }
